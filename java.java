337ee6de0d585bb5892652d3053eec6450530892
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-02-14 10:21:15 +0530

    project

---
+++ java.java
@@ -0,0 +1,14 @@
+/**
+ * java
+ */
+public class java {
+
+    public static void main(String[] args) {
+        int a= 45;
+        int b = 34;
+        for(int i=0;i<2;i++){
+            System.out.println(i);
+        }
+
+    }
+}
